5629168e2f54b449eec36f65a4227bbc462ed9a7
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-01-01 00:52:41 +0000

    Add personal settings to main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ mod trainer;
 use trainer::{lr, wdl, DirectSequentialDataLoader, LocalSettings, Trainer, TrainingSchedule, TrainingSteps};
 
 // Network architecture settings
-pub type InputFeatures = inputs::Chess768;
+pub type InputFeatures = inputs::ChessBucketsMirrored;
 pub type OutputBuckets = outputs::Single;
 pub type Activation = activation::SCReLU;
-pub const HL_SIZE: usize = 128;
+pub const HL_SIZE: usize = 1024;
 
 // Quantisations
 pub const QA: i16 = 255;
@@ -22,35 +22,35 @@ pub const QB: i16 = 64;
 pub const BUCKETS_MIRRORED: [usize; 32] = [
     0, 0, 0, 0,
     0, 0, 0, 0,
-    1, 1, 1, 1,
-    1, 1, 1, 1,
-    1, 1, 1, 1,
-    1, 1, 1, 1,
-    1, 1, 1, 1,
-    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
 ];
 
 fn main() {
     let mut trainer = Trainer::default();
     // let mut trainer = Trainer::from_checkpoint("checkpoints/legacy-10").unwrap();
 
-    let loader = DirectSequentialDataLoader::new(&["data/baseline.data"]);
+    let loader = DirectSequentialDataLoader::new(&["/Users/kelseyde/git/dan/calvin/data/calvindata.bin"]);
 
     let schedule = TrainingSchedule {
-        net_id: "legacy".to_string(),
+        net_id: "calvin1024_2b".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 20,
+            end_superbatch: 800,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 0.75 },
-        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 8 },
-        save_rate: 10,
+        wdl_scheduler: wdl::ConstantWDL { value: 0.4 },
+        lr_scheduler: lr::LinearDecayLR { initial_lr: 0.001, final_lr: 0.000027, final_superbatch: 800 },
+        save_rate: 2,
     };
 
-    let settings = LocalSettings { threads: 6, output_directory: "checkpoints", batch_queue_size: 64 };
+    let settings = LocalSettings { threads: 10, output_directory: "checkpoints", batch_queue_size: 64 };
 
     trainer.run(loader, &schedule, &settings);
 }
