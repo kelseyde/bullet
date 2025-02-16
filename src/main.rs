83c07b7f734fc14fb40a5d270bb0e36b026e13d0
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-02-16 11:30:33 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ mod trainer;
 use trainer::{lr, wdl, DirectSequentialDataLoader, LocalSettings, Trainer, TrainingSchedule, TrainingSteps};
 
 // Network architecture settings
-pub type InputFeatures = inputs::Chess768;
+pub type InputFeatures = inputs::ChessBucketsMirrored;
 pub type OutputBuckets = outputs::Single;
 pub type Activation = activation::SCReLU;
-pub const HL_SIZE: usize = 64;
+pub const HL_SIZE: usize = 1280;
 
 // Quantisations
 pub const QA: i16 = 255;
@@ -34,10 +34,13 @@ fn main() {
     let mut trainer = Trainer::default();
     // let mut trainer = Trainer::from_checkpoint("checkpoints/calvin1024_0wdl-430").unwrap();
 
-    let loader = DirectSequentialDataLoader::new(&["/Users/kelseyde/git/dan/calvin/data/calvindata.bin"]);
+    let loader = DirectSequentialDataLoader::new(&[
+        "/Users/kelseyde/git/dan/calvin/data/calvindata_1.bin",
+        "/Users/kelseyde/git/dan/calvin/data/calvindata_2.bin"
+    ]);
 
     let schedule = TrainingSchedule {
-        net_id: "calvin-test_8w".to_string(),
+        net_id: "calvin1280".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
@@ -45,7 +48,7 @@ fn main() {
             start_superbatch: 1,
             end_superbatch: 100,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 0.8 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.4 },
         lr_scheduler: lr::LinearDecayLR { initial_lr: 0.001, final_lr: 0.000027, final_superbatch: 800 },
         save_rate: 10,
     };
