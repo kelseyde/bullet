e21f0765284d6c21177ff691e03c8e96933e2310
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-03-25 19:37:10 +0100

    8 buckets

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use trainer::{lr, wdl, DirectSequentialDataLoader, LocalSettings, Trainer, Train
 pub type InputFeatures = inputs::ChessBucketsMirrored;
 pub type OutputBuckets = outputs::Single;
 pub type Activation = activation::SCReLU;
-pub const HL_SIZE: usize = 1024;
+pub const HL_SIZE: usize = 1280;
 
 // Quantisations
 pub const QA: i16 = 255;
@@ -20,14 +20,14 @@ pub const QB: i16 = 64;
 /// Indexed from white POV, so index 0 corresponds to A1, 3 corresponds to D1.
 #[rustfmt::skip]
 pub const BUCKETS_MIRRORED: [usize; 32] = [
-    0, 0, 1, 1,
-    2, 2, 2, 2,
-    3, 3, 3, 3,
-    3, 3, 3, 3,
-    3, 3, 3, 3,
-    3, 3, 3, 3,
-    3, 3, 3, 3,
-    3, 3, 3, 3,
+    0, 1, 2, 3,
+    4, 4, 5, 5,
+    6, 6, 6, 6,
+    6, 6, 6, 6,
+    6, 6, 6, 6,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
 ];
 
 fn main() {
@@ -40,15 +40,15 @@ fn main() {
     ]);
 
     let schedule = TrainingSchedule {
-        net_id: "calvin1280".to_string(),
+        net_id: "calvin1024_8b".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
-            start_superbatch: 711,
+            start_superbatch: 1,
             end_superbatch: 800,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.4 },
         lr_scheduler: lr::LinearDecayLR { initial_lr: 0.001, final_lr: 0.000027, final_superbatch: 800 },
         save_rate: 10,
     };
