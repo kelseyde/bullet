72ad0134e5c76e8744c9ddbb707fb28e22209b23
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-04-19 13:25:28 +0200

    Update simple.rs to match Calvin net arch and training schedule

--- examples/simple.rs
+++ examples/simple.rs
@@ -19,8 +19,9 @@ use bullet_lib::{
         settings::LocalSettings,
     },
 };
+use bullet_lib::default::inputs::ChessBucketsMirroredFactorised;
 
-const HIDDEN_SIZE: usize = 128;
+const HIDDEN_SIZE: usize = 1024;
 const SCALE: i32 = 400;
 const QA: i16 = 255;
 const QB: i16 = 64;
@@ -30,49 +31,42 @@ fn main() {
         .quantisations(&[QA, QB])
         .optimiser(optimiser::AdamW)
         .loss_fn(Loss::SigmoidMSE)
-        .input(inputs::Chess768)
+        .input(ChessBucketsMirroredFactorised::new([
+            0, 0, 1, 1,
+            2, 2, 2, 2,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+        ]))
         .output_buckets(outputs::Single)
         .feature_transformer(HIDDEN_SIZE)
-        .activate(Activation::CReLU)
+        .activate(Activation::SCReLU)
         .add_layer(1)
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "simple".to_string(),
+        net_id: "calvin_1024_4b_2".to_string(),
         eval_scale: SCALE as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 40,
+            end_superbatch: 800,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 0.75 },
-        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 18 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.4 },
+        lr_scheduler: lr::LinearDecayLR { initial_lr: 0.001, final_lr: 0.000027, final_superbatch: 800 },
         save_rate: 10,
     };
+
 
     trainer.set_optimiser_params(optimiser::AdamWParams::default());
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 64 };
 
-    // loading from a SF binpack
-    let _data_loader = {
-        let file_path = "data/test80-2024-02-feb-2tb7p.min-v2.v6.binpack";
-        let buffer_size_mb = 1024;
-        let threads = 4;
-        fn filter(entry: &TrainingDataEntry) -> bool {
-            entry.ply >= 16
-                && !entry.pos.is_checked(entry.pos.side_to_move())
-                && entry.score.unsigned_abs() <= 10000
-                && entry.mv.mtype() == MoveType::Normal
-                && entry.pos.piece_at(entry.mv.to()).piece_type() == PieceType::None
-        }
-
-        loader::SfBinpackLoader::new(file_path, buffer_size_mb, threads, filter)
-    };
-
-    // loading directly from a `BulletFormat` file
-    let data_loader = loader::DirectSequentialDataLoader::new(&["data/baseline.data"]);
+    let data_loader = loader::DirectSequentialDataLoader::new(&["data/calvin_data.bin"]);
 
     trainer.run(&schedule, &settings, &data_loader);
 }
