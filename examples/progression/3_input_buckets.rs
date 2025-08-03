f5be55ad8c8101d55e64c7dd2451d0be0bb43acc
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-08-03 15:44:29 +0200

    Update input buckets example

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -98,7 +98,7 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    let data_loader = loader::ViriBinpackLoader::new("/workspace/hobbes-234567.vf", 1024 * 8, 4, viriformat::dataformat::Filter::default());
+    let data_loader = loader::ViriBinpackLoader::new("/workspace/hobbes-2345678.vf", 1024 * 8, 4, viriformat::dataformat::Filter::default());
 
     trainer.run(&schedule, &settings, &data_loader);
 }
