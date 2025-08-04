676e877b660a67c803bf3cd36f6de7cdd6d4dc19
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-08-04 15:35:22 +0200

    Update input buckets example

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -81,7 +81,7 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "hobbes-13".to_string(),
+        net_id: "hobbes-13_2".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
