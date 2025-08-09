1346882078bee224782fc1bf5c77833230e8a9d3
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-08-09 13:52:37 +0200

    Update input buckets example

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -82,16 +82,16 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "hobbes-18".to_string(),
+        net_id: "hobbes-19".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 400,
+            end_superbatch: 800,
         },
         wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
-        lr_scheduler: lr::CosineDecayLR {initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 400},
+        lr_scheduler: lr::CosineDecayLR {initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800},
         save_rate: 10,
     };
 
