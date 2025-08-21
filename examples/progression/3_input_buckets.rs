318bc0811a8a1646c309e1db51c3200fd818bb8c
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-08-21 21:13:33 +0200

    Update dataset

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -97,7 +97,7 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    let data_loader = DirectSequentialDataLoader::new(&["/workspace/hobbes-6-to-20-shuffled.bin"]);
+    let data_loader = DirectSequentialDataLoader::new(&["/workspace/hobbes-6-to-21-shuffled.bin"]);
 
     trainer.run(&schedule, &settings, &data_loader);
 }
