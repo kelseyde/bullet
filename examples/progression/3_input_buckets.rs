ffceb34e8194a56c6c191f292414fe4abaab9304
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-16 19:39:33 +0200

    Update input buckets example

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -1,7 +1,6 @@
 use bullet_lib::{
     game::{
         inputs::{ChessBucketsMirrored, get_num_buckets},
-        outputs::MaterialCount,
     },
     nn::{
         InitSettings, Shape,
@@ -17,23 +16,18 @@ use bullet_lib::{
 
 fn main() {
     // hyperparams to fiddle with
-    let hl_size = 1024;
-    let dataset_path = "data/baseline.data";
-    let initial_lr = 0.001;
-    let final_lr = 0.001 * 0.3f32.powi(5);
-    let superbatches = 640;
-    let wdl_proportion = 0.75;
-    const NUM_OUTPUT_BUCKETS: usize = 8;
+    const HL_SIZE: usize = 1024;
+    const NUM_OUTPUT_BUCKETS: usize = 1;
     #[rustfmt::skip]
     const BUCKET_LAYOUT: [usize; 32] = [
         0, 1, 2, 3,
         4, 4, 5, 5,
         6, 6, 6, 6,
+        6, 6, 6, 6,
+        6, 6, 6, 6,
         7, 7, 7, 7,
-        8, 8, 8, 8,
-        8, 8, 8, 8,
-        9, 9, 9, 9,
-        9, 9, 9, 9,
+        7, 7, 7, 7,
+        7, 7, 7, 7,
     ];
 
     const NUM_INPUT_BUCKETS: usize = get_num_buckets(&BUCKET_LAYOUT);
@@ -42,7 +36,6 @@ fn main() {
         .dual_perspective()
         .optimiser(AdamW)
         .inputs(ChessBucketsMirrored::new(BUCKET_LAYOUT))
-        .output_buckets(MaterialCount::<NUM_OUTPUT_BUCKETS>)
         .save_format(&[
             // merge in the factoriser weights
             SavedFormat::id("l0w")
@@ -62,23 +55,23 @@ fn main() {
             SavedFormat::id("l1b").quantise::<i16>(255 * 64),
         ])
         .loss_fn(|output, target| output.sigmoid().squared_error(target))
-        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
+        .build(|builder, stm_inputs, ntm_inputs| {
             // input layer factoriser
-            let l0f = builder.new_weights("l0f", Shape::new(hl_size, 768), InitSettings::Zeroed);
+            let l0f = builder.new_weights("l0f", Shape::new(HL_SIZE, 768), InitSettings::Zeroed);
             let expanded_factoriser = l0f.repeat(NUM_INPUT_BUCKETS);
 
             // input layer weights
-            let mut l0 = builder.new_affine("l0", 768 * NUM_INPUT_BUCKETS, hl_size);
+            let mut l0 = builder.new_affine("l0", 768 * NUM_INPUT_BUCKETS, HL_SIZE);
             l0.weights = l0.weights + expanded_factoriser;
 
             // output layer weights
-            let l1 = builder.new_affine("l1", 2 * hl_size, NUM_OUTPUT_BUCKETS);
+            let l1 = builder.new_affine("l1", 2 * HL_SIZE, NUM_OUTPUT_BUCKETS);
 
             // inference
             let stm_hidden = l0.forward(stm_inputs).screlu();
             let ntm_hidden = l0.forward(ntm_inputs).screlu();
             let hidden_layer = stm_hidden.concat(ntm_hidden);
-            l1.forward(hidden_layer).select(output_buckets)
+            l1.forward(hidden_layer)
         });
 
     // need to account for factoriser weight magnitudes
@@ -87,22 +80,22 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "3_input_buckets".to_string(),
+        net_id: "hobbes-26".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: superbatches,
+            end_superbatch: 800,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: wdl_proportion },
-        lr_scheduler: lr::CosineDecayLR { initial_lr, final_lr, final_superbatch: superbatches },
+        wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
+        lr_scheduler: lr::CosineDecayLR {initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800},
         save_rate: 10,
     };
 
-    let settings = LocalSettings { threads: 2, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
+    let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    let dataloader = DirectSequentialDataLoader::new(&[dataset_path]);
+    let data_loader = DirectSequentialDataLoader::new(&["/workspace/hobbes-6-to-25-shuffled.bin"]);
 
-    trainer.run(&schedule, &settings, &dataloader);
+    trainer.run(&schedule, &settings, &data_loader);
 }
