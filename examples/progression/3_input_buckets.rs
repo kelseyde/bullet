a9efb68289cdfc744616a604b9cdac99d0360443
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-10-06 20:46:52 +0200

    Fine tuning stage

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -80,7 +80,7 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
     let schedule = TrainingSchedule {
-        net_id: "hobbes-28".to_string(),
+        net_id: "hobbes-29".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16_384,
@@ -88,8 +88,16 @@ fn main() {
             start_superbatch: 1,
             end_superbatch: 800,
         },
-        wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
-        lr_scheduler: lr::CosineDecayLR {initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800},
+        wdl_scheduler: wdl::Sequence {
+            first: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
+            first_scheduler_final_superbatch: 800,
+            second: wdl::ConstantWDL { value: 0.4 },
+        },
+        lr_scheduler: lr::Sequence {
+            first: lr::CosineDecayLR { initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800 },
+            first_scheduler_final_superbatch: 800,
+            second: lr::ConstantLR { value: 0.0000081 / 10.0 },
+        },
         save_rate: 10,
     };
 
