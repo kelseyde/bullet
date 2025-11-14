f0e57de49c731ac0b2ed4121e40ed167bfaf3773
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-11-14 08:51:02 +0100

    Piece count filter

--- examples/progression/3_input_buckets.rs
+++ examples/progression/3_input_buckets.rs
@@ -1,3 +1,7 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+use rand::{Rng, rng};
+use viriformat::chess::board::Board;
+use viriformat::chess::chessmove::Move;
 use bullet_lib::value::loader::ViriBinpackLoader;
 use bullet_lib::{
     game::inputs::{get_num_buckets, ChessBucketsMirrored},
@@ -12,7 +16,8 @@ use bullet_lib::{
     },
     value::ValueTrainerBuilder,
 };
-use viriformat::dataformat::Filter;
+use viriformat::dataformat::{Filter, WDL};
+use bullet_lib::value::loader::viribinpack::ViriFilter;
 
 fn main() {
     // hyperparams to fiddle with
@@ -99,8 +104,11 @@ fn main() {
 
     let settings = LocalSettings { threads: 12, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    let stage1_data_loader = ViriBinpackLoader::new("/workspace/data/hobbes-best.vf", 32768, 4, fen_skipping_filter(0.01));
-    let stage2_data_loader = ViriBinpackLoader::new("/workspace/data/hobbes-best.vf", 32768, 4, fen_skipping_filter(0.01));
+    let hobbes_all_path = "/workspace/data/hobbes-all.vf";
+    let hobbes_best_path = "/workspace/data/hobbes-best.vf";
+
+    let stage1_data_loader = ViriBinpackLoader::new(hobbes_all_path, 32768, 12, ViriFilter::Custom(filter));
+    let stage2_data_loader = ViriBinpackLoader::new(hobbes_best_path, 32768, 12, ViriFilter::Custom(filter));
 
     trainer.run(&stage_1_schedule, &settings, &stage1_data_loader);
     trainer.run(&stage_2_schedule, &settings, &stage2_data_loader);
@@ -116,10 +124,80 @@ fn training_steps(start_superbatch: usize, end_superbatch: usize) -> TrainingSte
     }
 }
 
+fn filter(board: &Board, mv: Move, eval: i16, wdl: f32) -> bool {
+    let default_viri_filter = Filter {
+        min_ply: 16,
+        min_pieces: 4,
+        max_eval: 31339,
+        filter_tactical: true,
+        filter_check: true,
+        filter_castling: false,
+        max_eval_incorrectness: u32::MAX,
+        random_fen_skipping: true,
+        random_fen_skip_probability: 0.75,
+        wdl_filtered: false,
+        wdl_model_params_a: [0.0; 4],
+        wdl_model_params_b: [0.0; 4],
+        material_min: 17,
+        material_max: 78,
+        mom_target: 58,
+        wdl_heuristic_scale: 1.5,
+    };
+    let mut rng = rng();
+    let wdl = match wdl {
+        1.0 => WDL::Win,
+        0.5 => WDL::Draw,
+        0.0 => WDL::Loss,
+        _ => unreachable!(),
+    };
+
+    !default_viri_filter.should_filter(mv, eval as i32, board, wdl, &mut rng)
+        && rng.random_bool(piece_count_acceptance(board))
+}
+
 fn fen_skipping_filter(probability: f64) -> Filter {
     Filter {
         random_fen_skipping: true,
         random_fen_skip_probability: probability,
         ..Default::default()
     }
+}
+
+fn piece_count_acceptance(board: &Board) -> f64 {
+    #[rustfmt::skip]
+    const DESIRED_DISTRIBUTION: [f64; 33] = [
+        0.018411966423, 0.020641545085, 0.022727271053,
+        0.024669162740, 0.026467201733, 0.028121406444,
+        0.029631758462, 0.030998276198, 0.032220941240,
+        0.033299772000, 0.034234750067, 0.035025893853,
+        0.035673184944, 0.036176641754, 0.036536245870,
+        0.036752015705, 0.036823932846, 0.036752015705,
+        0.036536245870, 0.036176641754, 0.035673184944,
+        0.035025893853, 0.034234750067, 0.033299772000,
+        0.032220941240, 0.030998276198, 0.029631758462,
+        0.028121406444, 0.026467201733, 0.024669162740,
+        0.022727271053, 0.020641545085, 0.018411966423,
+    ];
+
+    static PIECE_COUNT_STATS: [AtomicU64; 33] = [
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0),
+        AtomicU64::new(0),
+    ];
+    static PIECE_COUNT_TOTAL: AtomicU64 = AtomicU64::new(0);
+
+    let pc = board.pieces.occupied().count() as usize;
+    let count = PIECE_COUNT_STATS[pc].fetch_add(1, Ordering::Relaxed) + 1;
+    let total = PIECE_COUNT_TOTAL.fetch_add(1, Ordering::Relaxed) + 1;
+    let frequency = count as f64 / total as f64;
+
+    // Calculate the acceptance probability for this piece count
+    let acceptance = 0.5 * DESIRED_DISTRIBUTION[pc] / frequency;
+    acceptance.clamp(0., 1.)
 }
