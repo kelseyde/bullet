a8d781b35fc985261877cf781637022f13cc9edc
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-11-18 00:04:25 +0100

    Heimdall schedule

--- examples/progression/hobbes.rs
+++ examples/progression/hobbes.rs
@@ -13,11 +13,11 @@ use bullet_lib::{
     value::ValueTrainerBuilder,
 };
 use viriformat::dataformat::Filter;
-use bullet_lib::game::outputs::MaterialCount;
 
 fn main() {
     // hyperparams to fiddle with
     const HL_SIZE: usize = 1280;
+    const NUM_OUTPUT_BUCKETS: usize = 1;
     #[rustfmt::skip]
     const BUCKET_LAYOUT: [usize; 32] = [
          0,  1,  2,  3,
@@ -31,13 +31,11 @@ fn main() {
     ];
 
     const NUM_INPUT_BUCKETS: usize = get_num_buckets(&BUCKET_LAYOUT);
-    const NUM_OUTPUT_BUCKETS: usize = 8;
 
     let mut trainer = ValueTrainerBuilder::default()
         .dual_perspective()
         .optimiser(AdamW)
         .inputs(ChessBucketsMirrored::new(BUCKET_LAYOUT))
-        .output_buckets(MaterialCount::<NUM_OUTPUT_BUCKETS>)
         .save_format(&[
             // merge in the factoriser weights
             SavedFormat::id("l0w")
@@ -52,7 +50,7 @@ fn main() {
             SavedFormat::id("l1b").round().quantise::<i16>(255 * 64),
         ])
         .loss_fn(|output, target| output.sigmoid().squared_error(target))
-        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
+        .build(|builder, stm_inputs, ntm_inputs| {
             // input layer factoriser
             let l0f = builder.new_weights("l0f", Shape::new(HL_SIZE, 768), InitSettings::Zeroed);
             let expanded_factoriser = l0f.repeat(NUM_INPUT_BUCKETS);
@@ -68,7 +66,7 @@ fn main() {
             let stm_hidden = l0.forward(stm_inputs).screlu();
             let ntm_hidden = l0.forward(ntm_inputs).screlu();
             let hidden_layer = stm_hidden.concat(ntm_hidden);
-            l1.forward(hidden_layer).select(output_buckets)
+            l1.forward(hidden_layer)
         });
 
     // need to account for factoriser weight magnitudes
@@ -76,34 +74,69 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0w", stricter_clipping);
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
-    let stage_1_schedule = TrainingSchedule {
-        net_id: "hobbes-ob-s1".to_string(),
-        eval_scale: 400.0,
-        steps: training_steps(1, 800),
-        wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
-        lr_scheduler: lr::CosineDecayLR { initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800 },
-        save_rate: 10,
+    const STAGE1_SUPERBATCHES: usize = 100;
+    const STAGE2_SUPERBATCHES: usize = 800;
+    const STAGE3_SUPERBATCHES: usize = 100;
+    const LR_WARMUP_BATCHES: usize = 200;
+    const FIRST_WDL_FRACTION: f32 = 0.2;
+    const SECOND_WDL_FRACTION: (f32, f32) = (0.4, 0.6);
+    const THIRD_WDL_FRACTION: f32 = 0.8;
+    const FIRST_INITIAL_LR: f32 = 0.001;
+    const FIRST_FINAL_LR: f32 = 0.000027;
+    const SECOND_INITIAL_LR: f32 = 0.001;
+    const SECOND_FINAL_LR: f32 = 0.000027;
+    const THIRD_INITIAL_LR: f32 = 0.000025;
+    const THIRD_FINAL_LR: f32 = 0.0000025;
+
+    let wdl_scheduler = wdl::Sequence {
+        first: wdl::Sequence {
+            first: wdl::ConstantWDL { value: FIRST_WDL_FRACTION },
+            second: wdl::LinearWDL { start: SECOND_WDL_FRACTION.0, end: SECOND_WDL_FRACTION.1 },
+            first_scheduler_final_superbatch: STAGE1_SUPERBATCHES,
+        },
+        second: wdl::ConstantWDL { value: THIRD_WDL_FRACTION, },
+        first_scheduler_final_superbatch: STAGE1_SUPERBATCHES + STAGE2_SUPERBATCHES,
     };
 
-    let stage_2_schedule = TrainingSchedule {
-        net_id: "hobbes-ob-s2".to_string(),
+    let lr_scheduler = lr::Sequence {
+        first: lr::Sequence {
+            first: lr::Warmup {
+                inner: lr::LinearDecayLR {
+                    initial_lr: FIRST_INITIAL_LR,
+                    final_lr: FIRST_FINAL_LR,
+                    final_superbatch: STAGE1_SUPERBATCHES,
+                },
+                warmup_batches: LR_WARMUP_BATCHES,
+            },
+            second: lr::LinearDecayLR {
+                initial_lr: SECOND_INITIAL_LR,
+                final_lr: SECOND_FINAL_LR,
+                final_superbatch: STAGE2_SUPERBATCHES,
+            },
+            first_scheduler_final_superbatch: STAGE1_SUPERBATCHES,
+        },
+        second: lr::LinearDecayLR {
+            initial_lr: THIRD_INITIAL_LR,
+            final_lr: THIRD_FINAL_LR,
+            final_superbatch: STAGE3_SUPERBATCHES,
+        },
+        first_scheduler_final_superbatch: STAGE1_SUPERBATCHES + STAGE2_SUPERBATCHES,
+    };
+
+    let schedule = TrainingSchedule {
+        net_id: "hobbes-37-s1".to_string(),
         eval_scale: 400.0,
-        steps: training_steps(1, 200),
-        wdl_scheduler: wdl::ConstantWDL { value: 0.6 },
-        lr_scheduler: lr::ConstantLR { value: 0.00000081 },
+        steps: training_steps(1, 800),
+        wdl_scheduler,
+        lr_scheduler,
         save_rate: 10,
     };
 
     let settings = LocalSettings { threads: 12, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
+    let dataset_path = "/workspace/data/hobbes-all.vf";
+    let data_loader = ViriBinpackLoader::new(dataset_path, 32768, 24, filter());
 
-    let stage1_dataset_path = "/workspace/data/hobbes-all.vf";
-    let stage2_dataset_path = "/workspace/data/hobbes-best.vf";
-
-    let stage1_data_loader = ViriBinpackLoader::new(stage1_dataset_path, 32768, 24, filter());
-    let stage2_data_loader = ViriBinpackLoader::new(stage2_dataset_path, 32768, 24, filter());
-
-    trainer.run(&stage_1_schedule, &settings, &stage1_data_loader);
-    trainer.run(&stage_2_schedule, &settings, &stage2_data_loader);
+    trainer.run(&schedule, &settings, &data_loader);
     // space needed on cluster: 1.2TB BF
 }
 
