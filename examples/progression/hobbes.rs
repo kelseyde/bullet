8e9dc7f628d8ad113c6be251f088e5c98dea6f45
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-12-07 18:02:00 +0100

    Even moar wdl

--- examples/progression/hobbes.rs
+++ examples/progression/hobbes.rs
@@ -75,19 +75,19 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
 
     let stage_1_schedule = TrainingSchedule {
-        net_id: "hobbes-38-s1".to_string(),
+        net_id: "hobbes-40-s1".to_string(),
         eval_scale: 400.0,
         steps: training_steps(1, 800),
-        wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.4 } },
+        wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.3, end: 0.9 } },
         lr_scheduler: lr::CosineDecayLR { initial_lr: 0.001, final_lr: 0.0000081, final_superbatch: 800 },
         save_rate: 10,
     };
 
     let stage_2_schedule = TrainingSchedule {
-        net_id: "hobbes-38-s2".to_string(),
+        net_id: "hobbes-40-s2".to_string(),
         eval_scale: 400.0,
         steps: training_steps(1, 200),
-        wdl_scheduler: wdl::LinearWDL { start: 0.6, end: 1.0 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.9 },
         lr_scheduler: lr::ConstantLR { value: 0.00000081 },
         save_rate: 10,
     };
