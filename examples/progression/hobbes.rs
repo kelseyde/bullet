4d0aa856b31e64ec7a6bb9503caf9c997ba88cda
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-03-29 10:02:38 +0200

    1536 hl

--- examples/progression/hobbes.rs
+++ examples/progression/hobbes.rs
@@ -115,7 +115,7 @@ fn main() {
     trainer.optimiser.set_params_for_weight("l1w", l1_clip);
 
     let stage_1_schedule = TrainingSchedule {
-        net_id: "hobbes-40-s1".to_string(),
+        net_id: "hobbes-41-s1".to_string(),
         eval_scale: 400.0,
         steps: training_steps(581, 800),
         wdl_scheduler: wdl::Warmup { warmup_batches: 100, inner: wdl::LinearWDL { start: 0.2, end: 0.6 } },
@@ -124,7 +124,7 @@ fn main() {
     };
 
     let stage_2_schedule = TrainingSchedule {
-        net_id: "hobbes-40-s2".to_string(),
+        net_id: "hobbes-41-s2".to_string(),
         eval_scale: 400.0,
         steps: training_steps(1, 200),
         wdl_scheduler: wdl::ConstantWDL { value: 0.75 },
